a7d15dbd3b7a989cbf67dbbb1a7d49f5b2dc588f
mbaula <markbaula.1@gmail.com>
2024-06-19 10:28:51 -0400

    get rdy for deploy

--- spotifyvault/src/config.ts
+++ spotifyvault/src/config.ts
@@ -1,3 +1,3 @@
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID || '';
-export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || '';
+export const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI || 'https://spotifyvault.netlify.app';
 export const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET || '';
